7fcb060c2e92faae7cb397c41a9fcd61cb53c543
John Indra <i.john@me.com>
2022-03-26 18:13:02 +0700

    Tell SQLite not to check same thread
    We will be providing different connection object via FastAPI's dependency

--- db.py
+++ db.py
@@ -1,7 +1,7 @@
 from sqlalchemy.ext.asyncio import AsyncConnection, create_async_engine
 
 DATABASE_URL = "sqlite+aiosqlite:///./main.db"
-engine = create_async_engine(DATABASE_URL, echo=True)
+engine = create_async_engine(DATABASE_URL, connect_args={"check_same_thread": False}, echo=True)
 
 
 async def get_connection() -> AsyncConnection:
